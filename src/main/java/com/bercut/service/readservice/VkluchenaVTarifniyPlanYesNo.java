9f089d82ede55088aadb123691c73221cd9a3a95
Alyona Zakharova <alenazakharovaspb@yandex.ru>
2021-06-16 20:13:49 +0300

    removed redundant files

--- src/main/java/com/bercut/service/readservice/VkluchenaVTarifniyPlanYesNo.java
+++
@@ -1,5 +0,0 @@
-package com.bercut.service.readservice;
-
-public class VkluchenaVTarifniyPlanYesNo {
-    protected boolean vkljuchena_v_tarifnyj_plan_yesNo_value;
-}
--- src/main/java/com/bercut/service/utils/Helper.java
+++
@@ -1,8 +0,0 @@
-package com.bercut.service.utils;
-
-public class Helper {
-    public static boolean optionalParameterCheck(Boolean param) {
-        //param is true by default
-        return param == null || param;
-    }
-}
